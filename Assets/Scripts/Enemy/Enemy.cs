f57b27e525ea21bc9660856030a70bd99f7cf8bf
rumkkee <accesareo@gmail.com>
2024-03-05 15:39:22 -0800

    Cleaned EnemyHiveBrain and UI

--- Assets/Scripts/Enemy/Enemy.cs
+++ Assets/Scripts/Enemy/Enemy.cs
@@ -9,21 +9,14 @@ public class Enemy : MonoBehaviour
     public delegate void EnemyDefeated(int points);
     public static event EnemyDefeated OnDefeat;
 
-    public delegate void HitSideWall(Vector2 wallNormal);
-    public static event HitSideWall OnWallHit;
-
     public void Defeat()
     {
-        OnDefeat(_pointValue);
+        DeathCalls();
         Destroy(this.gameObject);
     }
 
-    private void OnCollisionEnter2D(Collision2D other)
+    public virtual void DeathCalls()
     {
-        SideWall wall = other.gameObject.GetComponent<SideWall>();
-        if(wall != null)
-        {
-            OnWallHit(wall.normal);
-        }
+        OnDefeat(_pointValue);
     }
 }
--- Assets/Scripts/Enemy/HiveEnemy.cs
+++ Assets/Scripts/Enemy/HiveEnemy.cs
@@ -7,8 +7,21 @@ public class HiveEnemy : Enemy
     public delegate void HiveEnemyDestroyed(Enemy enemy);
     public static event HiveEnemyDestroyed OnHiveEnemyDestroyed;
 
-    private void OnDestroy()
+    public delegate void HitSideWall(Vector2 wallNormal);
+    public static event HitSideWall OnWallHit;
+
+    private void OnCollisionEnter2D(Collision2D other)
     {
+        SideWall wall = other.gameObject.GetComponent<SideWall>();
+        if (wall != null)
+        {
+            OnWallHit(wall.normal);
+        }
+    }
+
+    public override void DeathCalls()
+    {
+        base.DeathCalls();
         OnHiveEnemyDestroyed(this);
     }
 }
